81d71eafea15ecc4ad068be9bcf148eb05cadb14
Robin <liugui@miya818.com>
2022-03-16 08:29:16 +0800

    hot setting

--- blog-service/main.go
+++ blog-service/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 	"time"
 
 	"github.com/go-programming-tour-book/blog-service/pkg/tracer"
@@ -16,6 +23,17 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+var (
+	port      string
+	runMode   string
+	config    string
+	isVersion bool
+
+	buildTime    string
+	buildVersion string
+	gitCommitID  string
+)
+
 func init() {
 	err := setupSetting()
 	if err != nil {
@@ -25,6 +43,17 @@ func init() {
 	if err != nil {
 		log.Fatalf("init.setupLogger err: %v", err)
 	}
+	setupFlag()
+}
+
+func setupFlag() error {
+	flag.StringVar(&port, "port", "", "启动端口")
+	flag.StringVar(&runMode, "mode", "", "启动模式")
+	flag.StringVar(&config, "config", "configs/", "指定要使用的配置文件路径")
+	flag.BoolVar(&isVersion, "version", false, "编译信息")
+	flag.Parse()
+
+	return nil
 }
 
 // @title 博客系统
@@ -35,6 +64,13 @@ func main() {
 	//ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	//global.Logger.Infof(ctx, "%s: go-programming-tour-book %s", "eddycjy","blog-service")
 
+	if isVersion {
+		fmt.Printf("build_time: %s\n", buildTime)
+		fmt.Printf("build_version: %s\n", buildVersion)
+		fmt.Printf("git_commit_id: %s\n", gitCommitID)
+		return
+	}
+
 	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
@@ -44,11 +80,32 @@ func main() {
 		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	s.ListenAndServe()
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("s.ListenAndServe err: %v", err)
+		}
+	}()
+
+	// 等待中断信号
+	quit := make(chan os.Signal)
+	// 接收syscall.SIGINT和syscall.SIGTERM信号
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shuting down server...")
+
+	// 最大时间控制，用于通知该服务端它有5秒的时间来处理原有的请求
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
 
 func setupSetting() error {
-	setting, err := setting.NewSetting()
+	setting, err := setting.NewSetting(strings.Split(config, ",")...)
 	if err != nil {
 		return err
 	}
